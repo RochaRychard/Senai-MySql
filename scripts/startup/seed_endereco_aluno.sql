2238b9f9fcf29c408f81821a20d954db45e5b842
Rychard Rocha <rocharychard31@gmail.com>
2022-08-19 19:40:48 -0300

    Adiciona endereço do aluno.

---
+++ scripts/startup/seed_endereco_aluno.sql
@@ -0,0 +1,3 @@
+INSERT INTO endereco (logradouro, numero, bairro, cep, uf_id, municipio_id) VALUES ('Avenida Claudio Manoel da costa', 1625, 'Interlagos', '29936-714', 68, 1),
+(' Av. Jerônimo Monteiro', 21, 'centro', '29450-000', 68, 8)
+
