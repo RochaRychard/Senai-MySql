716ff2729aaee259e2b0aaa95da23933f6110eeb
Rychard Rocha <rocharychard31@gmail.com>
2022-08-19 20:06:41 -0300

    Cadastro de alunos

---
+++ scripts/startup/seed_alunos.sql
@@ -0,0 +1,4 @@
+INSERT INTO aluno (matricula, nome, cpf, telefone, email, data_nascimento, nome_mae, nome_pai, endereco_id) 
+VALUES (33, 'João de Souza', '326.595.789-45', '27 98956-4267', 'joao@email.com', '1979:05:23', 'Joana de Souza', 'Paulo Sauro de Souza', 1),
+(34, 'Maria de Souza', '128.595.289-95', '27 98923-4977', 'maria@email.com', '1999:03:12', 'Joana de Souza', 'Paulo Sauro de Souza', 2)
+
